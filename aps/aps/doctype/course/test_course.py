81d91daf399eb9cb40c56afcee82a29439e1c3d4
mohaltheeb <altheebinfo@gmail.com>
2024-01-20 01:38:44 +0300

    fix(core):Deleted Course Doctype

--- aps/aps/doctype/course/test_course.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Mohammed Altheeb and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestCourse(FrappeTestCase):
-	pass
