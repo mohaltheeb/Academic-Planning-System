441d7794ea4bc0cd9789fcc72f7f9915466a6186
mohaltheeb <altheebinfo@gmail.com>
2024-01-20 02:33:57 +0300

    feat(core):Initialize Program, Faculty and University Requirements Doctypes without fields and functions

---
+++ aps/academic_planning/doctype/university_requirements/test_university_requirements.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Mohammed Altheeb and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestUniversityRequirements(FrappeTestCase):
+	pass
